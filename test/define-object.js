24d808f8b037efbc0ac56e848f38e113795f2b4f
Rodrigo Ribeiro <rodrigo.rs1989@gmail.com>
2017-05-13 20:56:17 -0300

    finalizando implementacao do defineObject

---
+++ test/define-object.js
@@ -0,0 +1,37 @@
+let assert = require('assert');
+let safira = require('../lib/Safira');
+
+describe('#Test Define Object',() => {
+    it('#defining json',() => {
+        let json = {name:'Maria',age:25};
+        safira.defineObject(json,'person');
+
+        let person = safira.bean('person');
+
+        assert(person.name === 'Maria');
+        assert(person.age === 25);
+    });
+
+    it('#defining json singleton',() => {
+        let json = {name:'Maria',age:25};
+        safira.defineObject(json,'person');
+
+        let person1 = safira.bean('person');
+        let person2 = safira.bean('person');
+
+        assert(person1 === person2);
+    });
+
+    it('#error defining object without name',() => {
+           
+        try{
+
+            let json = {name:'Maria',age:25};
+            safira.defineObject(json);
+        }
+        catch(e){
+            assert(e.message === 'Object name is required');
+        }
+
+    });
+});
